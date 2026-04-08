9195502df0f20e7d6a1eae961aff7ee275bc4a39
Murilo Melo <murilomelomd@gmail.com>
2026-04-08 09:25:16 -0300

    adicionando novo projeto spring

---
+++ src/main/java/br/com/garra/GarraApplication.java
@@ -0,0 +1,13 @@
+package br.com.garra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/garra/GarraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.garra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
